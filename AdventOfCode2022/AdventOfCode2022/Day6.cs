d49e211846ee7d19e6b8fc86336a42bc79b60160
lolombaba <lolo_cj@msn.com>
2022-12-07 20:47:54 +0100

    day6

---
+++ AdventOfCode2022/AdventOfCode2022/Day6.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2022
+{
+    public class Day6
+    {
+        string _value;
+
+        public Day6()
+        {
+            Part1();
+            Part2();
+        }
+
+        public void Init()
+        {
+            _value = File.ReadLines("../../../inputs/input_day6.txt").ToList()[0];
+        }
+
+        public void Part1()
+        {
+            Init();
+
+            var list = new List<char>();
+
+            for (int i = 0; i < _value.Length; i++)
+            {
+                for (int j = i; j < i + 4; j++)
+                    list.Add(_value[j]);
+
+                if (list.Distinct().Count() == 4)
+                {
+                    Console.WriteLine(i + 4);
+                    break;
+                }
+                else
+                {
+                    list.Clear();
+                }
+            }
+        }
+
+        public void Part2()
+        {
+            Init();
+
+            var list = new List<char>();
+
+            for (int i = 0; i < _value.Length; i++)
+            {
+                for (int j = i; j < i + 14; j++)
+                    list.Add(_value[j]);
+
+                if (list.Distinct().Count() == 14)
+                {
+                    Console.WriteLine(i + 14);
+                    break;
+                }
+                else
+                {
+                    list.Clear();
+                }
+            }
+        }
+    }
+}
--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -10,7 +10,8 @@ namespace AdventOfCode2022
             //var day2 = new Day2();
             //var day3 = new Day3();
             //var day4 = new Day4();
-            var day5 = new Day5();
+            //var day5 = new Day5();
+            var day6 = new Day6();
 
             Console.ReadLine();
         }
